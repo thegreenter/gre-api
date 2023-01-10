3c65691bf606b0b70eead1a269ddc85c5fc5cdaa
Giancarlos Salas <me@giansalex.dev>
2023-01-10 16:13:07 -0500

    Update user-agent

--- src/Configuration.php
+++ src/Configuration.php
@@ -101,7 +101,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'OpenAPI-Generator/1.0.0/PHP';
+    protected $userAgent = 'Greenter-Client/1.0.0/PHP';
 
     /**
      * Debug switch (default set to false)
